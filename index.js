ab20cf4401f3aefd947b3dfe4a55f048fff619ce
Jota <damatojoel25@gmail.com>
2024-11-07 18:44:32 -0300

    fixed name values

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const cors = require('cors');  // Añadir el paquete de CORS
-
-// Inicializar el servidor
-const app = express();
-const PORT = 3000;
-
-
-// Middleware para manejar CORS
-app.use(cors());  // Permitir todas las solicitudes de cualquier origen
-
-// Middleware para manejar datos en formato JSON
-app.use(bodyParser.json());
-
-
-// Token de Notion y ID de la base de datos
-const notionToken = 'secret_uCBoeC7cnlFtq7VG4Dr58nBYFLFbR6dKzF00fZt2dq';
-const notionDatabaseId = 'e1c86c0d490c4ccdb7b3d92007dea981';
-
-// Ruta para recibir los webhooks de Callbell
 app.post('/webhook/callbell', async (req, res) => {
     try {
-        const { sender_name, sender_phone } = req.body;
+        const { name, phoneNumber } = req.body;
 
         // Verificar que tenemos los datos requeridos
-        if (!sender_phone) {
+        if (!phoneNumber) {
             return res.status(400).send('Número de teléfono no proporcionado');
         }
 
@@ -37,13 +15,13 @@ app.post('/webhook/callbell', async (req, res) => {
                     title: [
                         {
                             text: {
-                                content: sender_name || "Sin Nombre"
+                                content: name || "Sin Nombre"
                             }
                         }
                     ]
                 },
                 Telefono: {
-                    phone_number: sender_phone
+                    phone_number: phoneNumber
                 },
                 Estado: {
                     select: {
@@ -57,7 +35,6 @@ app.post('/webhook/callbell', async (req, res) => {
                         }
                     ]
                 }
-                
             }
         }, {
             headers: {
@@ -72,9 +49,4 @@ app.post('/webhook/callbell', async (req, res) => {
         console.error('Error al guardar los datos en Notion:', error);
         res.status(500).send('Error al procesar el webhook');
     }
-});
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
